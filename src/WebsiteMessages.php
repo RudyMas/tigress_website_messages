5c531923dae74e94fa050f7f31ce6553c8ebd46e
RudyMasGoNext <rudy.mas@go-next.be>
2026-09-18 13:13:15 +0200

    version 2026.09.18

---
+++ src/WebsiteMessages.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tigress;
+
+/**
+ * Class WebsiteMessages (PHP version 8.5)
+ *
+ * @author Rudy Mas <rudy.mas@rudymas.be>
+ * @copyright 2026 Rudy Mas (https://rudymas.be)
+ * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @version 2026.09.18.0
+ * @package Controller\WebsiteMessages
+ */
+class WebsiteMessages
+{
+    /**
+     * @return string
+     */
+    public static function version(): string
+    {
+        return '2026.09.18';
+    }
+}
